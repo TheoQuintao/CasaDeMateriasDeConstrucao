434b1a82507b3a93df2656e0e744de96389ebea9
Theo Quintao <0000900198@senaimgaluno.com.br>
2025-04-17 09:12:51 -0300

    Index quase pronto

---
+++ CasaMateriasDeConstrucao/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapGet("/", async context =>
+{
+await context.Response.SendFileAsync("wwwroot/index.html");
+});
+
+app.Run();
